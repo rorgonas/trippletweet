3fc41d2a00346c63237c4f9ea9b0fe8fbe413565
Rolland I. Orgonas <rorgonas@gmail.com>
2015-04-06 23:56:16 -0400

    Add more flex over process env port

--- server/app.js
+++ server/app.js
@@ -11,7 +11,9 @@ app.use(bodyParser.json());
 app.use("/", express.static(__dirname + "/../client"));
 app.use('/api', require('./controllers/api'));
 
+// sets port 8080 to default or unless otherwise specified in the environment
+app.set('port', process.env.PORT || 8080);
 
-
-app.listen(3000);
-console.log('API is running on port 3000');
+// app.listen(3000);
+app.listen(app.get('port'));
+console.log('API is running on port ' + app.get('port'));
