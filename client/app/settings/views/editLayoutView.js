1d140692ae75b2be542e3c9a91b7546bf132f941
Rolland I. Orgonas <rorgonas@gmail.com>
2015-04-07 23:18:19 -0400

    Layout Settings: create events hash for edit and save

--- client/app/settings/views/editLayoutView.js
+++ client/app/settings/views/editLayoutView.js
@@ -10,15 +10,27 @@ define([
 		el: '#settings',
 		template: Handlebars.compile(EditLayoutTemplate),
 
+		events: {
+            'click button.edit': 'editSettings',
+            'click button.save': 'saveSettings'
+        },
+
 		initialize: function(){
 			console.log('Render aside panel for Layout Settings');
 			this.render();
 		},
 		render: function(){
-			//var attributes = this.model.attributes;
 			this.$el.html(this.template());
 			return this;
-		} 
+		},
+		editSettings: function(e){
+			// enter in edit mode. read from model/localstorage
+			console.log('Layout Settings:edit');
+		},
+		saveSettings: function(e){
+			// save changes into model/localstorage
+			console.log('Layout Settings:save');
+		}
 	});
 
   	return EditLayoutView;
